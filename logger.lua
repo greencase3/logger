2acb1ca753e1b24edf651a8b0c79a4bc7191b04c
greensky <102775197+greencase3@users.noreply.github.com>
2023-04-18 07:13:12 +0900

    Update logger.lua

--- logger.lua
+++ logger.lua
@@ -3,6 +3,7 @@ require "natives-1627063482"
 function playerActionsSetup(pid)
   local ip = players.get_connect_ip(pid)
   local name = players.get_name(pid)
+  local os = os.date("%Y-%m-%d %H:%M:%S")
   local id = tostring(name)
   local get_ips= string.format("[%i.%i.%i.%i]", ip >> 24 & 255, ip >> 16 & 255, ip >> 8 & 255, ip & 255)
 
@@ -10,7 +11,8 @@ function playerActionsSetup(pid)
     local filename = filesystem.store_dir() .. "logg.txt"
     local file = io.open(filename, "a")
   
-    file:write("Player " .. id .. " [error]\n")
+    file:write("[" ..os.."]".."Player " .. id .. " [error]\n")
+    util.toast("Player " .. id .. " [error]\n",TOAST_CONSOLE)
     file:close()
     return
   end
@@ -30,10 +32,11 @@ function playerActionsSetup(pid)
   if not lines[get_ips] then
     local file = io.open(filename, "a") 
 
-    file:write("Player " .. id .. " " ..get_ips .. "\n")
+    file:write("[" ..os.."]".."Player " .. id .. " " ..get_ips .. "\n")
+    util.toast("Player " .. id .. " " ..get_ips .. "\n",TOAST_CONSOLE)
     file:close()
   end
 end
 
-players.dispatch_on_join()
-players.on_join(playerActionsSetup)
+  players.dispatch_on_join()
+  players.on_join(playerActionsSetup)
