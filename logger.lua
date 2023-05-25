6e073ef9aaa6c73f345318964fa1e5ff06ebcc73
Greensky <102775197+notgreensky@users.noreply.github.com>
2023-05-25 09:07:04 +0900

    Update logger.lua

--- logger.lua
+++ logger.lua
@@ -1,22 +1,19 @@
-require "natives-1627063482"
-
 function playerActionsSetup(pid)
   local ip = players.get_connect_ip(pid)
   local name = players.get_name(pid)
   local os = os.date("%Y-%m-%d %H:%M:%S")
-  local id = tostring(name)
   local get_ips = string.format("[%i.%i.%i.%i]", ip >> 24 & 255, ip >> 16 & 255, ip >> 8 & 255, ip & 255)
   local vpn = players.is_using_vpn(pid)
 
   if get_ips == "[255.255.255.255]" then
     local filename = filesystem.store_dir() .. "logg.txt"
     local file = io.open(filename, "a")
-  
-    file:write("[" ..os.."]".."Player " .. id .. " [error]\n")
+ 
+    file:write("[" ..os.."]".."Player " .. name .. " [error]\n")
     file:close()
     return
   end
-
+  
   local filename = filesystem.store_dir() .. "logg.txt"
   local file = io.open(filename, "r")
   local lines = {}
@@ -30,15 +27,15 @@ function playerActionsSetup(pid)
     file:close()
   end
 
-  if name == players.get_name(players.user())  then
+if name == players.get_name(players.user()) then
     return
-  else if not lines[get_ips] then
+else if not lines[get_ips] then
     local file = io.open(filename, "a") 
-    file:write("[" ..os.."]".."Player " .. id .. " " ..get_ips .."[VPN="..vpn.."]".. "\n")
+    file:write("[" ..os.."]".."Player " .. name .. " " ..get_ips .."[VPN="..vpn.."]".. "\n")
      file:close()
-    end
+   end
   end
 end
 
-  players.dispatch_on_join()
-  players.on_join(playerActionsSetup)
+players.dispatch_on_join()
+players.on_join(playerActionsSetup)
